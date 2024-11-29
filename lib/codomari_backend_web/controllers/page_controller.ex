3f4aac97ccbc0d806d4d233716702b1dab373a83
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2024-11-29 10:34:15 +0000

    added manifest file

--- lib/codomari_backend_web/controllers/page_controller.ex
+++ lib/codomari_backend_web/controllers/page_controller.ex
@@ -1,9 +1,19 @@
 defmodule CodomariBackendWeb.PageController do
   use CodomariBackendWeb, :controller
 
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+  def manifest(conn, _params) do
+    manifest_path = Path.join(:code.priv_dir(:codomari_backend), "../codomari_backend.manifest")
+
+    case File.read(manifest_path) do
+      {:ok, content} ->
+        case Jason.decode(content) do
+          {:ok, data} -> json(conn, data)
+          {:error, _} -> send_resp(conn, 500, "Invalid manifest JSON format")
+        end
+
+      {:error, _reason} ->
+        send_resp(conn, 500, "Manifest file not found")
+    end
   end
 end
+
